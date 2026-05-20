8e11020550555e7e4a4b6d32180dc14cfdbbf022
ugcwbhumika11-alt <ugcwbhumika11@gmail.com>
2026-05-20 13:34:54 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,12 @@ const CACHE_NAME = "resume-app-v1";
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(["/", "/index.html", "/manifest.json"]);
+    return cache.addAll([
+  '/resume-builder-app/',
+  '/resume-builder-app/index.html',
+  '/resume-builder-app/manifest.json',
+  '/resume-builder-app/sw.js'
+]);
     })
   );
 });

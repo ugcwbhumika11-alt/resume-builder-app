1929e0d56772befa5f1cd5aeb837d819417713bb
ugcwbhumika11-alt <ugcwbhumika11@gmail.com>
2026-05-20 13:32:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,12 @@ const CACHE_NAME = 'my-app-v1';
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(['/', '/index.html']);
+    return cache.addAll([
+  '/resume-builder-app/',
+  '/resume-builder-app/index.html',
+  '/resume-builder-app/manifest.json',
+  '/resume-builder-app/sw.js'
+]);
     })
   );
 });
